538e511b58ef615df58b9a25a3f0d53d540aafc2
Cauterization <71918928+Cauterization@users.noreply.github.com>
2023-03-03 17:21:33 +0300

    Update deps

--- src/ENCOINS/ENCS/Distribution/IO.hs
+++ src/ENCOINS/ENCS/Distribution/IO.hs
@@ -12,21 +12,19 @@
 
 module ENCOINS.ENCS.Distribution.IO where
 
-import           Cardano.Api                      (NetworkId (..), StakeAddress, writeFileJSON, TxId)
-import qualified Cardano.Api
-import           Control.Monad                    (void, zipWithM)
-import           Data.Aeson                       (FromJSON, ToJSON, eitherDecodeFileStrict)
-import           Data.Function                    (on)
-import           Data.List                        (sortBy)
-import           Data.Ord                         (Down (..))
-import           Data.Tuple                       (swap)
-import           GHC.Generics                     (Generic)
-import           Ledger.Address                   (Address (..))
-import           Prelude                          hiding (Num (..), sum)
-
-import           ENCOINS.ENCS.OnChain             (ENCSParams, encsCurrencySymbol, encsTokenName)
-import           PlutusAppsExtra.IO.Blockfrost    (getAddressFromStakeAddress, verifyAssetFast)
-import           PlutusAppsExtra.Utils.Address    (addressToBech32)
+import           Cardano.Api                   (NetworkId (..), StakeAddress, TxId, writeFileJSON)
+import           Control.Monad                 (void)
+import           Data.Aeson                    (FromJSON, ToJSON, eitherDecodeFileStrict)
+import           Data.Function                 (on)
+import           Data.List                     (sortBy)
+import           Data.Ord                      (Down (..))
+import           Data.Tuple                    (swap)
+import           ENCOINS.ENCS.OnChain          (ENCSParams, encsCurrencySymbol, encsTokenName)
+import           GHC.Generics                  (Generic)
+import           Ledger.Address                (Address (..))
+import           PlutusAppsExtra.IO.Blockfrost (getAddressFromStakeAddress, verifyAssetFast)
+import           PlutusAppsExtra.Utils.Address (addressToBech32)
+import           Prelude                       hiding (Num (..), sum)
 
 data RawDistribution = RawDistribution
     { addressC :: StakeAddress
@@ -37,17 +35,14 @@ prepareDistribution :: NetworkId -> FilePath -> FilePath -> IO ()
 prepareDistribution networkId from to = do
     lst <- sortBy (compare `on` Down . rewardC) <$> (eitherDecodeFileStrict from >>= either fail pure)
     preparedD <- mapM (\d -> fmap swap . sequence . (rewardC d,) . (>>= addressToBech32 networkId) 
-        <$> getAddressFromStakeAddress (addressC d)) lst
+        <$> getAddressFromStakeAddress networkId (addressC d)) lst
     void $ writeFileJSON to $ sequence preparedD
 
--- verifyDistribution :: ENCSParams -> [(Address, Integer)] -> IO (Either Address [(Address, Integer, Cardano.Api.TxId)])
--- verifyDistribution par d = do
---     txIds <- mapM (\(address, amt) -> verifyAsset (encsCurrencySymbol par) encsTokenName amt address) d
---     pure $ zipWithM (\(address, amt) mbTxId -> maybe (Left address) (Right . (address, amt,)) mbTxId) d txIds
-
 verifyDistribution 
-    :: ENCSParams 
+    :: NetworkId
+    -> ENCSParams 
     -> [(Address, Integer)]
     -> Maybe ([(Address, Integer, TxId)] -> IO ()) -- Function to save intermidiate results
+    -> [(Address, Integer, Cardano.Api.TxId)]      -- Already verified addresses
     -> IO [Either Address (Address, Integer, TxId)]
-verifyDistribution par = verifyAssetFast (encsCurrencySymbol par) encsTokenName
+verifyDistribution network par = verifyAssetFast network (encsCurrencySymbol par) encsTokenName
